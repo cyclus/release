53bf70fae02e38c2cac3dca16e1816315a2a0493
Zach Welch <zwelch@wisc.edu>
2013-06-19 17:30:06 -0500

    Successful build for Ubuntu 10 64bit

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
+
 mkdir install
 
 
-
-cd ../glibmm-2.4.0
+cd glibmm-2.4.0
 ./configure --prefix=`pwd`/../install
 make
 make install
@@ -15,11 +15,15 @@ cd ../libxml++-2.30.0
 make
 make install
 
-cd coin-Cbc
+cd ../ boost_1_42_0/
+./bootstrap.sh
+./bjam install --prefix=`pwd`/../install
+
+
+cd ../coin-Cbc
 ./configure --prefix=`pwd`/../install
 make 
 make install
-
 
 cd ../cyclopts
 cmake src/ -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCOIN_ROOT_DIR=`pwd`/../install
@@ -27,9 +31,8 @@ make
 make install 
 
 
-
 cd ../cyclus
-cmake src -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCYCLOPTS_ROOT_DIR=`pwd`/../install -DCOIN_ROOT_DIR=`pwd`../install
+cmake src -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCYCLOPTS_ROOT_DIR=`pwd`/../install -DCOIN_ROOT_DIR=`pwd`/../install -DBOOST_ROOT=`pwd`/../install
 make
 make install
 

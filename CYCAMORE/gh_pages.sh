5596d59c9476705466d034ff1ce769a9fadd30b5
Zach Welch <zwelch@wisc.edu>
2013-10-25 09:54:50 -0500

    setting up for nightly push

--- CYCAMORE/gh_pages.sh
+++ CYCAMORE/gh_pages.sh
@@ -1,15 +1,21 @@
-
-
+#!/bin/bash
+set -x
+set -e
 #`pwd`/../CYCLUS/gh_pages.sh
 
 
 #cd cycamore
 #git checkout gh-pages
 ls -l
-cd cycamore
-git checkout gh-pages
-return 0
+tar -xzf results.tar.gz
 
+cd cycamore
+ls
+git checkout gh-pages
+rsync -a ../cycamoredoc/* .
+git add -A
+git commit -m "nightly build"
+git push ssh://git@github.com/zwelchWI/cycamore.git develop
 
 
 

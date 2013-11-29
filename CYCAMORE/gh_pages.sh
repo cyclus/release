0bc2b5fe9f6df675f427f0d15cc7ef04e7120ca4
Zach Welch <zwelch@wisc.edu>
2013-11-29 10:29:44 -0600

    fixes nightly build skip

--- CYCAMORE/gh_pages.sh
+++ CYCAMORE/gh_pages.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -x
 set -e
 
 
+if [ ! -e results.tar.gz ]
+then
+exit 0
+fi
 tar -xzf results.tar.gz
 ls -l
 
@@ -10,8 +14,11 @@ cd cyclus
 git checkout gh-pages
 rsync -a ../cycamoredoc/html/* api/
 git add -A
-git commit -m "nightly build"
+val=`git commit -m "nightly build" | wc -l`
+if [ $val == 0 ]
+then
 git push -f ssh://git@github.com/cyclus/cyclus.git gh-pages
+fi
 cd ..
 
 
@@ -19,8 +26,10 @@ cd cycamore
 git checkout gh-pages
 rsync -a ../cycamoredoc/html/* api/
 git add -A
-git commit -m "nightly build"
+val=`git commit -m "nightly build" | wc -l`
+if [ $val == 0 ]
+then
+
 git push -f ssh://git@github.com/cyclus/cycamore.git gh-pages
-
-
+fi
 

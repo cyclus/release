d27889e54dcdfda3180a1ed8ec90a0aef9cec189
Zach Welch <zwelch@wisc.edu>
2013-12-02 11:26:24 -0600

    Fast version of cyclus run with google test fix

--- CYCLUS/run_test.fast.sh
+++ CYCLUS/run_test.fast.sh
@@ -2,12 +2,6 @@ set -x
 set -e
 export DYLD_LIBRARY_PATH=`pwd`/install/lib:$DYLD_LIBRARY_PATH
 
-if [ -e install/cyclus/bin/CycamoreUnitTestDriver ]
-then
-    install/cycamore/bin/CycamoreUnitTestDriver --gtest_repeat=1
-else
-    install/bin/CycamoreUnitTestDriver --gtest_repeat=1
-fi
 if [ -e install/cyclus/bin/cyclus_unit_tests ]
 then
     install/cyclus/bin/cyclus_unit_tests --gtest_repeat=1

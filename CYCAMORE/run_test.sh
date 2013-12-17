eb6a1a4e8bea9dfab616847421e7757663edc267
Anthony Scopatz <scopatz@gmail.com>
2013-12-17 11:54:42 -0600

    unit test driver name change

--- CYCAMORE/run_test.sh
+++ CYCAMORE/run_test.sh
@@ -6,11 +6,11 @@ export DYLD_LIBRARY_PATH=`pwd`/install/lib:$DYLD_LIBRARY_PATH
 
 if  [[ "${_NMI_TASKNAME}" == CYCAMORE* ]]
 then
-    if [ -e install/cyclus/bin/CycamoreUnitTestDriver ]
+    if [ -e install/cyclus/bin/cycamore_unit_tests ]
     then
-    install/cycamore/bin/CycamoreUnitTestDriver --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCAMORE.//g'`
+    install/cycamore/bin/cycamore_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCAMORE.//g'`
     else
-    install/bin/CycamoreUnitTestDriver --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCAMORE.//g'`
+    install/bin/cycamore_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCAMORE.//g'`
     fi
 else
     if [ -e install/cyclus/bin/cyclus_unit_tests ]

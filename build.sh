cab2a5a60adb92194ae617221a785856a214a3c3
Zach Welch <zwelch@wisc.edu>
2014-03-19 09:52:58 -0500

    test

--- build.sh
+++ build.sh
@@ -7,16 +7,18 @@ anaconda/bin/conda install binstar
 anaconda/bin/conda install conda-build
 anaconda/bin/conda install jinja2
 anaconda/bin/conda install setuptools
-#anaconda/bin/conda install sigcpp
-#anaconda/bin/conda install glibmm
-#anaconda/bin/conda  install libxmlpp
-#anaconda/bin/conda  install coincbc
-#anaconda/bin/conda install boost
-#anaconda/bin/conda install hdf5
-#anaconda/bin/conda install libxml2
-#anaconda/bin/conda install cmake
-#anaconda/bin/conda build --no-test --no-binstar-upload cyclus
-#tar -czf results.tar.gz anaconda
+anaconda/bin/conda install sigcpp
+anaconda/bin/conda install glibmm
+anaconda/bin/conda  install libxmlpp
+anaconda/bin/conda  install coincbc
+anaconda/bin/conda install boost
+anaconda/bin/conda install hdf5
+anaconda/bin/conda install libxml2
+anaconda/bin/conda install cmake
+anaconda/bin/conda build --no-test --no-binstar-upload cyclus
+tar -czf results.tar.gz anaconda
 
-anaconda/bin/conda install cyclus
-
+#anaconda/bin/conda install cyclus
+ls -l anaconda
+ls -l anaconda/bin
+ls -l anaconda/share

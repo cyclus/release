5c33faa91800f82402091642174f2049af5b9f6e
Zach Welch <zwelch@wisc.edu>
2014-03-20 20:24:04 -0500

    fixed

--- cyclus/build.sh
+++ cyclus/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-python install.py --prefix=$PREFIX
+export LD_LIBRARY_PATH=$PREFIX/lib/
+export CMAKE_LIBRARY_PATH=$PREFIX/lib/
+export PATH=$PREFIX/bin:$PATH
+echo HERE
+echo $LD_LIBRARY_PATH
+ls -l $PREFIX
+ls -l $PREFIX/lib
 
+cmake  -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX  -DBOOST_LIBRARYDIR=$PREFIX/lib -DBoost_NO_SYSTEM_PATHS=ON
+make
+make install

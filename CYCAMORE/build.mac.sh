499081b9f170c3af551dc506fe13d39b99b1e5c8
gidden <matthew.gidden@gmail.com>
2014-04-07 12:44:48 -0500

    preserved original mac cmake build

--- CYCAMORE/build.mac.sh
+++ CYCAMORE/build.mac.sh
@@ -5,9 +5,7 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:`pwd`/install/lib/
 
 `pwd`/CYCLUS/build.mac.sh
 cd cycamore
-mkdir build
-cd build
-cmake ../src -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCYCLUS_ROOT_DIR=`pwd`/../install  -DCYCLOPTS_ROOT_DIR=`pwd`/../install -DCOIN_ROOT_DIR=`pwd`/../install -DBOOST_ROOT=`pwd`/../install
+cmake src -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCYCLUS_ROOT_DIR=`pwd`/../install  -DCYCLOPTS_ROOT_DIR=`pwd`/../install -DCOIN_ROOT_DIR=`pwd`/../install -DBOOST_ROOT=`pwd`/../install
 make
 make install
 cd ..

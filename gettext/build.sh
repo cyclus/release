7bc45a9967844900d0e9b2977cf7eecc6af83c2c
Zach Welch <zwelch@wisc.edu>
2014-04-16 21:41:12 -0500

    working but possibly broken gettext

--- gettext/build.sh
+++ gettext/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
-
-
-
-./configure --prefix=$PREFIX
+#set -x 
+#set -e
+./configure --prefix=$PREFIX --disable-java  --enable-fast-install
 make
+#ls 
+#mv bin $PREFIX/bin
+#mv include $PREFIX/include
+#mv lib $PREFIX/lib
+#mv share $PREFIX/share
 make install
-
+rm -rf $PREFIX/share/doc/gettext/examples/
+rm -rf $PREFIX/share/gettext/
 

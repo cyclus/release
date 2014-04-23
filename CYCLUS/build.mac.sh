c0d366e3410d8419b22d6d1643e40ad94aaf8323
Matthew Gidden <matthew.gidden@gmail.com>
2014-04-23 11:23:46 -0500

    added python builds

--- CYCLUS/build.mac.sh
+++ CYCLUS/build.mac.sh
@@ -96,6 +96,19 @@ cmake `pwd` -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCYCLOPTS_ROOT_DIR=`pwd`/..
 make
 make install
 cd ..
+mkdir -p `pwd`/install/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:`pwd`/install:`pwd`/install/lib/python2.7/site-packages
+cd nose
+python setup.py install --prefix=`pwd`/../install
+cd ../numpy
+python setup.py install --prefix=`pwd`/../install
+cd ../cython
+python setup.py install --prefix=`pwd`/../install
+cd ../numexpr
+python setup.py install --prefix=`pwd`/../install
+cd ../PyTables
+python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
+cd ..
 
 exit $?
 

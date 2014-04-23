3bed719bf4d5d2042be42a6498e1f2d62192f1e1
Matthew Gidden <matthew.gidden@gmail.com>
2014-04-23 12:06:52 -0500

    added paths

--- CYCLUS/run_test.fast.sh
+++ CYCLUS/run_test.fast.sh
@@ -2,6 +2,8 @@ set -x
 set -e
 export DYLD_LIBRARY_PATH=`pwd`/install/lib:$DYLD_LIBRARY_PATH
 export PATH=$PATH:`pwd`/install/bin
+export PYTHONPATH=$PYTHONPATH:`pwd`/install:`pwd`/install/lib/python2.7/site-packages
+export LD_LIBRARY_PATH=`pwd`/install/lib/:$LD_LIBRARY_PATH
 
 cyclus_unit_tests --gtest_repeat=1
 `pwd`/install/bin/nosetests -w `pwd`/cyclus/tests

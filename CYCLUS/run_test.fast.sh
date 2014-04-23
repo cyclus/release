8f08ffc27dbc5f18508f6f5f58c24523ed2e5f03
Matthew Gidden <matthew.gidden@gmail.com>
2014-04-23 10:48:37 -0500

    updated the fast run tests to run nosetests

--- CYCLUS/run_test.fast.sh
+++ CYCLUS/run_test.fast.sh
@@ -1,14 +1,10 @@
 set -x
 set -e
 export DYLD_LIBRARY_PATH=`pwd`/install/lib:$DYLD_LIBRARY_PATH
+export PATH=$PATH:`pwd`/install/bin
 
-if [ -e install/cyclus/bin/cyclus_unit_tests ]
-then
-    install/cyclus/bin/cyclus_unit_tests --gtest_repeat=1
-else
-    install/bin/cyclus_unit_tests --gtest_repeat=1
-fi
-
+cyclus_unit_tests --gtest_repeat=1
+nosetests -w `pwd`/cyclus/tests
 
 
 exit $?

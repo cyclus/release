43b423af8072ed6f50ca524200b791f4b60cbaa5
Matthew Gidden <matthew.gidden@gmail.com>
2014-04-23 11:08:45 -0500

    made call to nosetests explicit

--- CYCLUS/run_test.fast.sh
+++ CYCLUS/run_test.fast.sh
@@ -4,7 +4,7 @@ export DYLD_LIBRARY_PATH=`pwd`/install/lib:$DYLD_LIBRARY_PATH
 export PATH=$PATH:`pwd`/install/bin
 
 cyclus_unit_tests --gtest_repeat=1
-nosetests -w `pwd`/cyclus/tests
+`pwd`/install/bin/nosetests -w `pwd`/cyclus/tests
 
 
 exit $?

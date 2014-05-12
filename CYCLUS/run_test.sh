0c54a008e92baa373930864210af8112b2c1f45f
Matthew Gidden <matthew.gidden@gmail.com>
2014-05-12 08:45:38 -0500

    simplified nightly test scripts, cyclus now added to PATH

--- CYCLUS/run_test.sh
+++ CYCLUS/run_test.sh
@@ -1,20 +1,15 @@
 set -x
 set -e
 export DYLD_LIBRARY_PATH=`pwd`/install/lib:$DYLD_LIBRARY_PATH
+export PYTHONPATH=$PYTHONPATH:`pwd`/install:`pwd`/install/lib/python2.7/site-packages
+export LD_LIBRARY_PATH=`pwd`/install/lib/:$LD_LIBRARY_PATH    
+export PATH=$PWD/install/bin/:$PWD/install/cyclus/bin/:$PATH
 
 echo "Querying cyclus version"
 cyclus --version
 
+cyclus_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCLUS.//g'`
 
-if [ -e install/cyclus/bin/cyclus_unit_tests ]
-then
-    install/cyclus/bin/cyclus_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCLUS.//g'`
-else
-    install/bin/cyclus_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCLUS.//g'`
-fi
-
-`pwd`/install/bin/nosetests -w `pwd`/cyclus/tests
-
-
+nosetests -w `pwd`/cyclus/tests
 
 exit $?

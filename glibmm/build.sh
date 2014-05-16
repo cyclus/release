38a84972cd6e8facce1b2de8f90a3c30079ea351
Zach Welch <zwelch@wisc.edu>
2014-05-16 17:39:15 -0500

    glibmm working

--- glibmm/build.sh
+++ glibmm/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -x
+set -e
 
-
-
-
-./configure --prefix=$PREFIX
+MACOSX_DEPLOYMENT_TARGET=
+export DYLD_LIBRARY_PATH=$PREFIX/lib
+./configure --prefix=$PREFIX PKG_CONFIG=$PREFIX/bin/pkg-config PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/ PATH=$PATH:$PREFIX/bin/ CPPFLAGS=-I/$PREFIX/include LDFLAGS=-L/$PREFIX/lib
 make
 make install
-
 

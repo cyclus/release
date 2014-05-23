ec4e387b8cab8f3b34f4552ac008998194f4649c
Zach Welch <zwelch@wisc.edu>
2014-05-23 15:41:58 -0500

    fix bug to run fast tests

--- CYCA/run_test.sh
+++ CYCA/run_test.sh
@@ -2,27 +2,15 @@ set -x
 set -e
 export DYLD_LIBRARY_PATH=`pwd`/anaconda/lib:`pwd`/anaconda/lib/cyclus:$DYLD_LIBRARY_PATH
 
-if [[ "${_NMI_TASKNAME}" == "" ]]
-then
-
-    anaconda/bin/cycamore_unit_tests --gtest_repeat=1
-    anaconda/bin/cyclus_unit_tests --gtest_repeat=1
     # check that unit tests ran
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
-else
-
-
-
     if  [[ "${_NMI_TASKNAME}" == CYCAMORE* ]]
     then
         anaconda/bin/cycamore_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCAMORE.//g'`
     elif [[ "${_NMI_TASKNAME}" == CYCLUS* ]]
     then
         anaconda/bin/cyclus_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CYCLUS.//g'`
-    else
+    elif [[  "${_NMI_TASKNAME}" == R*  ]]
+    then
     
         # run regression tests
         export PYTHONPATH=$PYTHONPATH:anaconda:anaconda/lib/python2.7/site-packages
@@ -30,11 +18,14 @@ else
         export PATH=anaconda/bin/:$PATH
         cd `pwd`/cycamore/tests
         ../../anaconda/bin/nosetests
-
-
-fi
-
-
+    else
+    anaconda/bin/cycamore_unit_tests --gtest_repeat=1
+    anaconda/bin/cyclus_unit_tests --gtest_repeat=1
+    # check that unit tests ran
+    if [ $? -ne 0 ]
+    then
+        exit $?
+    fi
 fi
 
 exit $?

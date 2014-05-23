508937c92b8e7d0f5df12d9eca131572bf03cfb1
Zach Welch <zwelch@wisc.edu>
2014-05-23 15:15:45 -0500

    working cyclus and cycamore

--- glibmm/build.sh
+++ glibmm/build.sh
@@ -2,9 +2,15 @@
 set -x
 set -e
 
-MACOSX_DEPLOYMENT_TARGET=
-export DYLD_LIBRARY_PATH=$PREFIX/lib
-./configure --prefix=$PREFIX PKG_CONFIG=$PREFIX/bin/pkg-config PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/ PATH=$PATH:$PREFIX/bin/ CPPFLAGS=-I/$PREFIX/include LDFLAGS=-L/$PREFIX/lib
+
+if [[  `uname` == 'Linux' ]]; then
+   ./configure --prefix=$PREFIX  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+else
+
+   MACOSX_DEPLOYMENT_TARGET=
+   export DYLD_LIBRARY_PATH=$PREFIX/lib
+   ./configure --prefix=$PREFIX PKG_CONFIG=$PREFIX/bin/pkg-config PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/ PATH=$PATH:$PREFIX/bin/ CPPFLAGS=-I/$PREFIX/include LDFLAGS=-L/$PREFIX/lib
+fi
 make
 make install
 

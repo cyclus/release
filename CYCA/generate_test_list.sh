60c2ff10e5a599d85ee69f3d64a6ef38b72eeff5
Zach Welch <zwelch@wisc.edu>
2014-05-27 15:37:37 -0500

    Fix test name generation error

--- CYCA/generate_test_list.sh
+++ CYCA/generate_test_list.sh
@@ -1,7 +1,7 @@
 set -x
 set -e
 export DYLD_LIBRARY_PATH=`pwd`/anaconda/lib:`pwd`/anaconda/lib/cyclus:$DYLD_LIBRARY_PATH
-
+export LD_LIBRARY_PATH=`pwd`/anaconda/lib:`pwd`/anaconda/lib/cyclus:$LD_LIBRARY_PATH
 
 anaconda/bin/cyclus_unit_tests --gtest_list_tests  | python gen_test_list.py CYCLUS.
 anaconda/bin/cyclus_unit_tests --gtest_list_tests  | python gen_test_list.py CYCLUS. > tasklist.nmi
@@ -12,6 +12,7 @@ anaconda/bin/cycamore_unit_tests --gtest_list_tests  | python gen_test_list.py C
 
 echo "Regression_Test" >> tasklist.nmi
 
+cat tasklist.nmi
 
 exit $?
 

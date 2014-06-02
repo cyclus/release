f82243f0caa2485bffa18a0adc08e40bffcf974b
Zach Welch <zwelch@wisc.edu>
2014-06-02 15:17:57 -0500

    Fix nightlies to handle out of source builds

--- CYCLUS/build.sh
+++ CYCLUS/build.sh
@@ -86,12 +86,6 @@ export CMAKE_LIBRARY_PATH=`pwd`/install/lib/:$CMAKE_LIBRARY_PATH
 export LD_RUN_PATH=`pwd`/lapack-3.2.1/:$LD_RUN_PATH
 echo $LD_LIBRARY_PATH
 
-cd doxygen
-./configure --prefix $MYINSTALL
-make
-make install
-cd ..
-
 cd cyclus
 
 find -exec touch \{\} \;

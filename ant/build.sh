defd894d84da5a72a06dcb09e664674d7aa7dc83
Anthony Scopatz <scopatz@gmail.com>
2014-10-13 10:10:02 -0500

    Ant seems to work on mac

--- ant/build.sh
+++ ant/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
 UNAME=`uname`
+BUILD_CACHE="$RECIPE_DIR/../build/cache"
+mkdir -p $BUILD_CACHE
 HAMCREST="hamcrest-core-1.3.jar"
 if [[ $UNAME == "Linux" ]]; then
   # Linux
@@ -16,7 +18,10 @@ echo "JAVA_HOME = ${JAVA_HOME}"
 
 # Build won't work without this installed. Potentially a bug.
 # see http://archive.linuxfromscratch.org/mail-archives/blfs-book/2014-May/042604.html
-curl -o "lib/optional/${HAMCREST}" "https://hamcrest.googlecode.com/files/${HAMCREST}"
+if [ ! -f $BUILD_CACHE/${HAMCREST} ]; then
+  curl -o "$BUILD_CACHE/${HAMCREST}" "https://hamcrest.googlecode.com/files/${HAMCREST}"
+fi
+cp -v "$BUILD_CACHE/${HAMCREST}" "lib/optional/${HAMCREST}"
 
 # Build
 sh build.sh -Ddist.dir=$PREFIX dist

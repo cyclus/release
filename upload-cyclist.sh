2406b48265184dd3a62be6b8717817e47a564a6a
Anthony Scopatz <scopatz@gmail.com>
2014-10-18 18:04:33 -0500

    upload cyclist

--- upload-cyclist.sh
+++ upload-cyclist.sh
@@ -1,25 +1,42 @@
-dir=$1
-version=$2
-tmpdir=.tmp_release_dir
+#!/bin/bash 
+set -e
+set -x
 
-mkdir $tmpdir
-cd $tmpdir
+# Setup binstar
+which binstar
+if [ $? -ne 0 ]; then
+  export PATH="${HOME}/miniconda/bin:${PATH}"
+fi
 
-mtar=$dir/userdir/nmi:x86_64_MacOSX8/results.tar.gz
-mkdir mac
-cp $mtar mac/
-cd mac
-binstar upload -u cyclus anaconda/conda-bld/osx-64/cyclus-$version.tar.bz2
-binstar upload -u cycamore anaconda/conda-bld/osx-64/cycamore-$version.tar.bz2
-cd ..
+ID="$1"
+IFS=':' read -ra RUNDIRS <<< $(nmi_rundir $ID)
+RUNDIR="${RUNDIR[2]}"
+USERDIR="${RUNDIR}/userdir"
 
-utar=$dir/userdir/nmi:x86_64_Ubuntu12/results.tar.gz
-mkdir ubuntu
-cp $utar ubuntu
-cd ubuntu
-binstar upload -u cyclus anaconda/conda-bld/linux-64/cyclus-$version.tar.bz2
-binstar upload -u cycamore anaconda/conda-bld/linux-64/cycamore-$version.tar.bz2
-cd ..
+for x in $(ls $USERDIR); do
+  if [ "$x" == "common" ]; then continue; fi
+  ls -l $x/results.*
+done
 
-cd ..
-rm -r $tmpdir
+
+#mkdir $tmpdir
+#cd $tmpdir
+
+#mtar=$dir/userdir/nmi:x86_64_MacOSX8/results.tar.gz
+#mkdir mac
+#cp $mtar mac/
+#cd mac
+#binstar upload -u cyclus anaconda/conda-bld/osx-64/cyclus-$version.tar.bz2
+#binstar upload -u cycamore anaconda/conda-bld/osx-64/cycamore-$version.tar.bz2
+#cd ..
+
+#utar=$dir/userdir/nmi:x86_64_Ubuntu12/results.tar.gz
+#mkdir ubuntu
+#cp $utar ubuntu
+#cd ubuntu
+#binstar upload -u cyclus anaconda/conda-bld/linux-64/cyclus-$version.tar.bz2
+#binstar upload -u cycamore anaconda/conda-bld/linux-64/cycamore-$version.tar.bz2
+#cd ..
+
+#cd ..
+#rm -r $tmpdir

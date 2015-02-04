09e378a973b91dabf11d794e070735c35f3eabf2
Anthony Scopatz <scopatz@gmail.com>
2015-02-04 12:03:51 -0600

    started cymetric conda recipe

---
+++ cymetric/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$PREFIX/lib/
+export CMAKE_LIBRARY_PATH=$PREFIX/lib/
+export PATH=$PREFIX/bin:$PATH
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/cyclus:$PREFIX/lib
+
+ln -s $PREFIX/lib/libhdf5.so.9 $PREFIX/lib/libhdf5.so.8
+ln -s $PREFIX/lib/libhdf5_hl.so.9 $PREFIX/lib/libhdf5_hl.so.8
+
+ldd $PREFIX/bin/cyclus_base
+
+$PREFIX/bin/cyclus --version
+python setup.py install --prefix=$PREFIX --build-type=Release --hdf5=$PREFIX

13049757810172a2aa528d73ac3e0d8bd13bb802
Zach Welch <zwelch@wisc.edu>
2015-03-02 13:51:41 -0600

    changes

--- CYM/run_test.sh
+++ CYM/run_test.sh
@@ -3,7 +3,20 @@ set -x
 set -e
 
 export PATH="$(pwd)/anaconda/bin:${PATH}"
+anaconda/bin/cyclus --install-path
+anaconda/bin/cyclus --nuc-data
+anaconda/bin/cyclus --version
+which cymetric
 
-anaconda/bin/nosetests -vsw cymtests
+which python
+echo "subprocess ---- "
+$(which python) -c "import subprocess; print(subprocess.__file__)"
+
+cd cymtests
+ls .
+conda install nose
+export PYTHONPATH="$(ls ../anaconda/lib/python*/site-packages):$PYTHONPAH"
+echo "PATH: ${PATH}"
+nosetests -vs
 
 exit $?

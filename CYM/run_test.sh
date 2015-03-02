5605ec9ee5debcbde38c0c8aeca2d81046da3227
Anthony Scopatz <scopatz@gmail.com>
2015-03-02 14:10:13 -0600

    mac hdf5 fix and sys.path

--- CYM/run_test.sh
+++ CYM/run_test.sh
@@ -17,6 +17,7 @@ ls .
 conda install nose
 export PYTHONPATH="$(ls ../anaconda/lib/python*/site-packages):$PYTHONPAH"
 echo "PATH: ${PATH}"
+echo "PYTHONPATH: $(python -c 'import sys; from pprint import pprint; pprint(sys.path)')"
 nosetests -vs
 
 exit $?

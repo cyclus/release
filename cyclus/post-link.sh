b86b27abf223c15347f634db1312cf50fff2f552
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-22 13:03:14 -0500

    adding sym link to post-link.sh

--- cyclus/post-link.sh
+++ cyclus/post-link.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
+# hack fix for hdf5 issues
+if [[  `uname` == 'Linux' ]]; then
+  ln -s $PREFIX/lib/libhdf5.so.9 $PREFIX/lib/libhdf5.so.8
+  ln -s $PREFIX/lib/libhdf5_hl.so.9 $PREFIX/lib/libhdf5_hl.so.8
+else
+  ln -s $PREFIX/lib/libhdf5.9.dylib $PREFIX/lib/libhdf5.8.dylib
+  ln -s $PREFIX/lib/libhdf5_hl.9.dylib $PREFIX/lib/libhdf5_hl.8.dylib
+fi
+
 mv $PREFIX/bin/cyclus $PREFIX/bin/cyclus_base
 echo "
 #!/bin/bash

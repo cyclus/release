259b819cb09e30e3592eaacb577119d0afa40a13
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-22 18:31:00 -0500

    matching cycamore build to cyclus

--- cycamore/build.sh
+++ cycamore/build.sh
@@ -6,24 +6,24 @@ export CMAKE_LIBRARY_PATH=$PREFIX/lib/
 export PATH=$PREFIX/bin:$PATH
 export MACOSX_DEPLOYMENT_TARGET=
 
-# hack fix for hdf5 issues
-if [[  `uname` == 'Linux' ]]; then
-  ln -s $PREFIX/lib/libhdf5.so.9 $PREFIX/lib/libhdf5.so.8
-  ln -s $PREFIX/lib/libhdf5_hl.so.9 $PREFIX/lib/libhdf5_hl.so.8
-else
-  ln -s $PREFIX/lib/libhdf5.9.dylib $PREFIX/lib/libhdf5.8.dylib
-  ln -s $PREFIX/lib/libhdf5_hl.9.dylib $PREFIX/lib/libhdf5_hl.8.dylib
-fi
+# # hack fix for hdf5 issues
+# if [[  `uname` == 'Linux' ]]; then
+#   ln -s $PREFIX/lib/libhdf5.so.9 $PREFIX/lib/libhdf5.so.8
+#   ln -s $PREFIX/lib/libhdf5_hl.so.9 $PREFIX/lib/libhdf5_hl.so.8
+# else
+#   ln -s $PREFIX/lib/libhdf5.9.dylib $PREFIX/lib/libhdf5.8.dylib
+#   ln -s $PREFIX/lib/libhdf5_hl.9.dylib $PREFIX/lib/libhdf5_hl.8.dylib
+# fi
 
 $PREFIX/bin/cyclus --version
 
 if [[  `uname` == 'Linux' ]]; then
 
-    cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX -DCYCLUS_ROOT_DIR=$PREFIX -DBOOST_ROOT=$PREFIX  -DBOOST_LIBRARYDIR=$PREFIX/lib -DBoost_NO_SYSTEM_PATHS=ON -DCMAKE_BUILD_TYPE=Release -DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.so -DBLAS_LIBRARIES=$PREFIX/lib/libblas.so
+    cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX -DCYCLUS_ROOT_DIR=$PREFIX -DHDF5_ROOT=$PREFIX -DBOOST_ROOT=$PREFIX  -DBOOST_LIBRARYDIR=$PREFIX/lib -DBoost_NO_SYSTEM_PATHS=ON -DCMAKE_BUILD_TYPE=Release -DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.so -DBLAS_LIBRARIES=$PREFIX/lib/libblas.so
 else
 
 export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/cyclus:$PREFIX/lib
-cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX  -DCYCLUS_ROOT_DIR=$PREFIX -DCOIN_ROOT_DIR=$PREFIX -DBOOST_ROOT=$PREFIX   -DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.dylib -DBLAS_LIBRARIES=$PREFIX/lib/libblas.dylib
+cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX  -DCYCLUS_ROOT_DIR=$PREFIX -DHDF5_ROOT=$PREFIX -DCOIN_ROOT_DIR=$PREFIX -DBOOST_ROOT=$PREFIX   -DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.dylib -DBLAS_LIBRARIES=$PREFIX/lib/libblas.dylib
 fi
 make 
 make install

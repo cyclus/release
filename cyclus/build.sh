3f0bc61362a5b74d69fa2e30a224b8e8defbeb6d
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-22 14:29:04 -0500

    adding -j flag

--- cyclus/build.sh
+++ cyclus/build.sh
@@ -5,6 +5,10 @@ cd build
 export LD_LIBRARY_PATH=$PREFIX/lib/
 export CMAKE_LIBRARY_PATH=$PREFIX/lib/
 export PATH=$PREFIX/bin:$PATH
+export INCLUDE_PATH=$PREFIX/include:$INCLUDE_PATH
+export C_INCLUDE_PATH=$PREFIX/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$PREFIX/include:$CPLUS_INCLUDE_PATH
+
 export MACOSX_DEPLOYMENT_TARGET=
 
 # # hack fix for hdf5 issues
@@ -18,6 +22,7 @@ export MACOSX_DEPLOYMENT_TARGET=
 
 if [[  `uname` == 'Linux' ]]; then
     cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX -DHDF5_ROOT=$PREFIX -DBOOST_ROOT=$PREFIX  -DBOOST_LIBRARYDIR=$PREFIX/lib -DBoost_NO_SYSTEM_PATHS=ON -DCMAKE_BUILD_TYPE=Release -DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.so -DBLAS_LIBRARIES=$PREFIX/lib/libblas.so
+    make VERBOSE=1 -j $((`nproc` * 2))
 else
 echo $CFLAGS
 echo $LDFLAGS
@@ -27,7 +32,7 @@ export LDFLAGS="-headerpad_max_install_names -headerpad"
 export CFLAGS="-headerpad_max_install_names -headerpad"
 export CXXFLAGS=
     cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX -DHDF5_ROOT=$PREFIX -DCOIN_ROOT_DIR=$PREFIX -DBOOST_ROOT=$PREFIX -DCMAKE_BUILD_TYPE=Release  -DLAPACK_LIBRARIES=$PREFIX/lib/liblapack.dylib -DBLAS_LIBRARIES=$PREFIX/lib/libblas.dylib
-fi
 make VERBOSE=1
+fi
 make install
 cd ..

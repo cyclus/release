8e452be1352bd5a82d5d3b7c0b150e51f3103be8
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2015-05-05 07:40:46 -0500

    * added usage() with consistent style to other scripts * changed env variables to be consistent with project naming

--- utils/make_release_notes.sh
+++ utils/make_release_notes.sh
@@ -2,19 +2,41 @@
 
 set -e
 
+usage() {
+
+   usage_text = <<EOF
+Script to generate release notes for the current release version relative to a previous version.
+
+usage: $0 <previous>  <version>
+
+       previous : the version string of the previous version (e.g., P.P.P) which provides the
+                  basis for determining what is new in the current release
+        version : the version string (e.g., V.V.V) to with which this package will be labeled.
+
+NOTE: both version strings must already be existing tags in both the Cyclus and Cycamore repositories.
+
+The following environment variables must be set for this script to function:
+
+     CYCLUS_DIR : Environment variable CYCLUS_DIR must be set to the cyclus repository directory.
+   CYCAMORE_DIR : Environment variable CYCAMORE_DIR must be set to the cycamore repository directory.
+
+EOF
+
+}
+
+
 die() {
     echo >&2 "$@"
+    usage()
     exit 1
 }
 
 HERE=$PWD
 
 # check input
-CORE=${CORE_DIR?"Environment variable CORE_DIR must be set to the cyclus repository directory."}
-CYCA=${CYCAMORE_DIR?"Environment variable CYCAMORE_DIR must be set to the cycamore repository directory."}
-[ "$#" -eq 2 ] || die "Must provide the from version and to version (e.g., W.W.W -> X.X.X) as an argument, usage:
-
-$ ./conda_upload.sh W.W.W X.X.X"
+CYCLUS=${CYCLUS_DIR?"Environment variable CYCLUS_DIR must be set to the cyclus repository directory."}
+CYCAMORE=${CYCAMORE_DIR?"Environment variable CYCAMORE_DIR must be set to the cycamore repository directory."}
+[ "$#" -eq 2 ] || die "Must provide the from version and to version (e.g., W.W.W -> X.X.X) as an argument"
 PREV=$1
 VERSION=$2
 echo "Making release notes template for Cyclus stack verison $VERSION from $PREV. 
@@ -23,30 +45,30 @@ Note, these version names must refer to *existing tags* in all repositories.
 "
 
 # cyclus summary
-cd $CORE
+cd $CYCLUS
 NCOMMITS=`git rev-list $PREV...$VERSION --count | tail -n1`
 SUMMARY=`git diff --stat $PREV...$VERSION | tail -n1`
-CORETXT="$NCOMMITS commits resulting in $SUMMARY"
-CORECONTRIB=`git log --format='%aN' $PREV...$VERSION | sort -u`
+CYCLUSTXT="$NCOMMITS commits resulting in $SUMMARY"
+CYCLUSCONTRIB=`git log --format='%aN' $PREV...$VERSION | sort -u`
 cd $HERE
 
 # cycamore summary
-cd $CYCA
+cd $CYCAMORE
 NCOMMITS=`git rev-list $PREV...$VERSION --count | tail -n1`
 SUMMARY=`git diff --stat $PREV...$VERSION | tail -n1`
-CYCATXT="$NCOMMITS commits resulting in $SUMMARY"
-CYCACONTRIB=`git log --format="%aN" $PREV...$VERSION | sort -u`
+CYCAMORETXT="$NCOMMITS commits resulting in $SUMMARY"
+CYCAMORECONTRIB=`git log --format="%aN" $PREV...$VERSION | sort -u`
 cd $HERE
 
 # contributors, beware, thar be hackery ahead
 echo "Raw core contributors:"
-echo "$CORECONTRIB"
+echo "$CYCLUSCONTRIB"
 echo ""
-echo "$CORECONTRIB" > .contribs
+echo "$CYCLUSCONTRIB" > .contribs
 echo "Raw cyca contributors:"
-echo "$CYCACONTRIB"
+echo "$CYCAMORECONTRIB"
 echo ""
-echo "$CYCACONTRIB" >> .contribs
+echo "$CYCAMORECONTRIB" >> .contribs
 CONTRIBTXT=`cat .contribs | sort -u | awk '{print "* " $0}'`
 echo "$CONTRIBTXT" > .contribs
 
@@ -55,8 +77,8 @@ FILE=release_notes.rst
 cp -i release_notes.rst.in $FILE
 sed -i "s/@PREV_VERSION@/$PREV/g" $FILE 
 sed -i "s/@VERSION@/$VERSION/g" $FILE 
-sed -i "s/@CORE_SUMMARY@/$CORETXT/g" $FILE 
-sed -i "s/@CYCA_SUMMARY@/$CYCATXT/g" $FILE 
+sed -i "s/@CYCLUS_SUMMARY@/$CYCLUSTXT/g" $FILE 
+sed -i "s/@CYCAMORE_SUMMARY@/$CYCAMORETXT/g" $FILE 
 sed -i '/@CONTRIBUTORS@/r .contribs' $FILE 
 sed -i '/@CONTRIBUTORS@/d' $FILE 
 rm .contribs

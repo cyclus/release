0a7650a2739c5e4503a475ac812c6122f0b88563
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2015-05-05 07:39:17 -0500

    * added usage() with consistent style to other scripts * changed env variables to be consistent with project naming * changed from git clone to wget for pyne retrieval

--- utils/maintenence.sh
+++ utils/maintenence.sh
@@ -2,46 +2,65 @@
 
 set -e
 
+usage() {
+
+   usage_text = <<EOF
+Script to perform maintenance tasks related to the release of Cyclus and Cycamore.
+
+usage: $0 <version>
+
+        version : the version string (e.g., X.Y.Z) to with which this package will be labeled.
+
+The following environment variables must be set for this script to function:
+
+     CYCLUS_DIR : Environment variable CYCLUS_DIR must be set to the cyclus repository directory.
+   CYCAMORE_DIR : Environment variable CYCAMORE_DIR must be set to the cycamore repository directory.
+    CYCSTUB_DIR : Environment variable CYCSTUB_DIR must be set to the cycstub repository directory.
+
+EOF
+
+}
+
 die() {
     echo >&2 "$@"
+    usage()
     exit 1
 }
 
 # check input
-CORE=${CORE_DIR?"Environment variable CORE_DIR must be set to the cyclus repository directory."}
-CYCA=${CYCAMORE_DIR?"Environment variable CYCAMORE_DIR must be set to the cycamore repository directory."}
-STUB=${STUB_DIR?"Environment variable STUB_DIR must be set to the cycstub repository directory."}
-[ "$#" -eq 1 ] || die "Must provide the version (e.g., X.X.X) as an argument, usage:
-
-$ ./conda_upload.sh X.X.X" 
+CYCLUS=${CYCLUS_DIR?"Environment variable CYCLUS_DIR must be set to the cyclus repository directory."}
+CYCAMORE=${CYCAMORE_DIR?"Environment variable CYCAMORE_DIR must be set to the cycamore repository directory."}
+CYCSTUB=${CYCSTUB_DIR?"Environment variable CYCSTUB_DIR must be set to the cycstub repository directory."}
+[ "$#" -eq 1 ] || die "Must provide the version (e.g., X.X.X) as an argument"
 VERSION=$1
 echo "Performing maintence updates for Cyclus stack verison $VERSION"
 
 # pyne
-git clone git@github.com:pyne/pyne
+wget https://github.com/pyne/pyne/archive/develop.zip
+unzip -d pyne develop.zip
 cd pyne
 ./amalgamate.py -s pyne.cc -i pyne.h
-cp pyne.* $CORE/src
+cp pyne.* $CYCLUS/src
 cd ..
-rm -rf pyne
+rm -rf pyne develop.zip
 
 # nuc_data upload
-cd $CORE/release
+cd $CYCLUS/release
 nuc_data_make -o cyclus_nuc_data.h5 \
     -m atomic_mass,scattering_lengths,decay,simple_xs,materials,eaf,wimsd_fpy,nds_fpy
 python upload_nuc_data.py
 rm -r build_nuc_data cyclus_nuc_data.h5
-cd $CORE
+cd $CYCLUS
 
 # cyclus
-cd $CORE/release
+cd $CYCLUS/release
 ./smbchk.py --update -t HEAD --no-save --check | grep "ABI stability has been achieved!"
 ./smbchk.py --update -t $VERSION
-cd $CORE
+cd $CYCLUS
 
 # cycstub
-cp $CORE/tests/input/stub_example.xml $STUB/input/example.xml
-cp $CORE/stubs/stub_* $STUB/src/
+cp $CYCLUS/tests/input/stub_example.xml $CYCSTUB/input/example.xml
+cp $CYCLUS/stubs/stub_* $CYCSTUB/src/
 
 echo "
 *-----------------------------------------------------------------------------*
